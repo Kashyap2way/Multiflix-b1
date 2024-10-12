7590232e9b3ae4c0c1c813316f0f401edfbc9bb5
Kashyap <kashyap.mistry.btech2021@sitpune.edu.in>
2024-10-12 19:31:59 +0530

    commit5

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
 const io = require("socket.io")();
 const crypto = require("crypto");
+const http = require("http");
 
 const parties = {};
 
 const generatePartyCode = () => {
-return crypto.randomBytes(3).toString('hex').toUpperCase();
+    return crypto.randomBytes(3).toString('hex').toUpperCase();
 };
 
 module.exports = async function (context, req) {
-if (!context.res) {
-    context.res = {};
-}
+    // Create an HTTP server
+    const server = http.createServer();
+    
+    // Attach Socket.IO to the HTTP server
+    io.attach(server);
 
-io.attach(context.res.socket.server);
+    io.on('connection', (socket) => {
+        console.log('New user connected');
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
+        socket.on('createParty', () => {
+            const partyId = generatePartyCode();
+            parties[partyId] = { users: [] };
 
-    socket.on('createParty', () => {
-    const partyId = generatePartyCode();
-    parties[partyId] = { users: [] };
+            socket.join(partyId);
+            parties[partyId].users.push(socket.id);
 
-    socket.join(partyId);
-    parties[partyId].users.push(socket.id);
+            socket.emit('partyCreated', { partyId });
+        });
 
-    socket.emit('partyCreated', { partyId });
+        socket.on('joinParty', (partyId) => {
+            if (parties[partyId]) {
+                socket.join(partyId);
+                parties[partyId].users.push(socket.id);
+                socket.emit('partyJoined', { success: true });
+            } else {
+                socket.emit('partyJoined', { success: false, message: "Party not found!" });
+            }
+        });
+
+        socket.on('videoAction', (data) => {
+            const { partyId, action, time } = data;
+
+            if (parties[partyId]) {
+                socket.to(partyId).emit('videoAction', { action, time });
+            }
+        });
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected');
+            for (const partyId in parties) {
+                parties[partyId].users = parties[partyId].users.filter(id => id !== socket.id);
+                if (parties[partyId].users.length === 0) {
+                    delete parties[partyId];
+                }
+            }
+        });
     });
 
-    socket.on('joinParty', (partyId) => {
-    if (parties[partyId]) {
-        socket.join(partyId);
-        parties[partyId].users.push(socket.id);
-        socket.emit('partyJoined', { success: true });
-    } else {
-        socket.emit('partyJoined', { success: false, message: "Party not found!" });
-    }
+    // Start the server
+    server.listen(0, () => {
+        context.res = {
+            status: 200,
+            body: "Socket.IO server is running"
+        };
     });
-
-    socket.on('videoAction', (data) => {
-    const { partyId, action, time } = data;
-
-    if (parties[partyId]) {
-        socket.to(partyId).emit('videoAction', { action, time });
-    }
-    });
-
-    socket.on('disconnect', () => {
-    console.log('User disconnected');
-    for (const partyId in parties) {
-        parties[partyId].users = parties[partyId].users.filter(id => id !== socket.id);
-        if (parties[partyId].users.length === 0) {
-        delete parties[partyId];
-        }
-    }
-    });
-});
-
-context.res = {
-    body: "Socket.IO server is running"
-};
 };
